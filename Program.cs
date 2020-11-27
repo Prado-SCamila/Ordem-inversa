b551e162bdeb2e9248a19459c0810d1afd00a10f
Prado-SCamila <prado.camila.910423@gmail.com>
2020-11-26 22:57:59 -0300

    Projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ordem_inversa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] numeros = new int[15];
+
+            for(var i=0; i<15; i++){
+            Console.WriteLine("Digite um numeros");
+            numeros[i] = int.Parse(Console.ReadLine());
+
+            }
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            for(var i=14; i>0;i--){
+            Console.WriteLine(numeros[i]);
+
+            }
+        }
+    }
+}
